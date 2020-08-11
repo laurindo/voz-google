596d8d7672e2092b0b095511ab60eb12b20fee96
Daniel <dslaurindo@gmail.com>
2020-08-11 13:50:56 -0300

    Updates

---
+++ precache-manifest.5ae65f022074948a5c9013ce05df1b30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6e4c0c1023770820c1849227fa1495",
+    "url": "/voz-google/index.html"
+  },
+  {
+    "revision": "ad7d57d3802ca5b72a12",
+    "url": "/voz-google/static/css/main.2a2cfb0a.chunk.css"
+  },
+  {
+    "revision": "fb82962f404659eb3e8a",
+    "url": "/voz-google/static/js/2.1cb1260e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/voz-google/static/js/2.1cb1260e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad7d57d3802ca5b72a12",
+    "url": "/voz-google/static/js/main.cddb3a50.chunk.js"
+  },
+  {
+    "revision": "892fbfcd5e1bafd88217",
+    "url": "/voz-google/static/js/runtime-main.27073961.js"
+  },
+  {
+    "revision": "6702341dc9da6b081405db7d965a1fc7",
+    "url": "/voz-google/static/media/voice.6702341d.png"
+  }
+]);
