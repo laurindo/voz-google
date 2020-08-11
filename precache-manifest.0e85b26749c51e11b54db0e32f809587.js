1d2d1e717a072c263ae9db302cb4bbeaa06c05ab
Daniel <dslaurindo@gmail.com>
2020-08-11 15:57:24 -0300

    Updates

---
+++ precache-manifest.0e85b26749c51e11b54db0e32f809587.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df7e9c8ae0a9f35562ac6c2a785df39",
+    "url": "/voz-google/index.html"
+  },
+  {
+    "revision": "5d07024892f7e131c549",
+    "url": "/voz-google/static/css/main.d3895824.chunk.css"
+  },
+  {
+    "revision": "f670a7d06eeb9f1d412f",
+    "url": "/voz-google/static/js/2.5a1aae07.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/voz-google/static/js/2.5a1aae07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d07024892f7e131c549",
+    "url": "/voz-google/static/js/main.3988528c.chunk.js"
+  },
+  {
+    "revision": "892fbfcd5e1bafd88217",
+    "url": "/voz-google/static/js/runtime-main.27073961.js"
+  },
+  {
+    "revision": "6702341dc9da6b081405db7d965a1fc7",
+    "url": "/voz-google/static/media/voice.6702341d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/voz-google/precache-manifest.8ee59128400c5471d0ba8c493b7814b4.js"
+  "/voz-google/precache-manifest.0e85b26749c51e11b54db0e32f809587.js"
 );
 
 self.addEventListener('message', (event) => {
